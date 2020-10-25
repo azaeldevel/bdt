5204a2c05e14b354790f28ee415f58f9be58dee8
Azael Reyes <azael.devel@gmail.com>
2020-10-25 01:36:35 -0700

    Agregando proceso de basic
    Se crea el sub-comando build iniciar la compilacion desde bdt
    
    Se agrega la opcion help

--- src/commands.cc
+++ src/commands.cc
@@ -44,14 +44,123 @@ int LFS::sync(int argc, char* argv[])
 	//std::cout << "Ejecutando : "<< cmd << "\n";
 	return system (cmd.c_str());
 }
-int LFS::remove(int argc, char* argv[])
+
+
+
+
+
+
+
+
+
+int Interpret::config3(int argc, char* argv[])
+{	
+	std::vector<coreutils::Enviroment*> venv;
+	coreutils::Enviroment* env = new coreutils::Enviroment();
+	env->name = "LFS";
+	env->value = header->getRootDir();
+	venv.push_back(env);env = new coreutils::Enviroment();
+	env->name = "REPO";
+	env->value = header->getRoot_Repository();
+	venv.push_back(env);
+	shell.set(venv);
+	
+	std::string cmd;
+	
+	
+	cmd = "/tools/bin/bash bdt-basichost-directories";
+	shell.execute(cmd);
+	cmd = "/tools/bin/bash bdt-basichost-populate2";
+	
+	
+	for(auto env : venv)
+	{
+		delete env;
+	}
+	return 0;
+}
+
+int Interpret::config2(int argc, char* argv[])
+{	
+	std::vector<coreutils::Enviroment*> venv;
+	coreutils::Enviroment* env = new coreutils::Enviroment();
+	env->name = "LFS";
+	env->value = header->getRootDir();
+	venv.push_back(env);
+	shell.set(venv);
+	
+	std::string cmd;
+	
+	
+	cmd = "/tools/bin/bash bdt-basichost-chroot";
+	shell.execute(cmd);
+	
+	
+	
+	delete env;
+	return 0;
+}
+
+int Interpret::config1(int argc, char* argv[])
+{	
+	std::vector<coreutils::Enviroment*> venv;
+	coreutils::Enviroment* env = new coreutils::Enviroment();
+	env->name = "LFS";
+	env->value = header->getRootDir();
+	venv.push_back(env);
+	shell.set(venv);
+	
+	std::string cmd;
+	
+	
+	cmd = "/tools/bin/bash bdt-basichost-populate";
+	shell.execute(cmd);
+	
+	
+	
+	delete env;
+	return 0;
+}
+int Interpret::build(int argc, char* argv[])
+{
+	if(argc < 1) return 1;//minimo se deve especificar la version
+		
+	std::string cmd;
+	
+	
+			
+	for(int i = 0; i < argc; i++)
+	{
+		if(strcmp(argv[i],"--version") == 0 and argv[i+1] != NULL and i+1 <= argc)
+		{
+			version.set(argv[i+1]);
+			i++;
+		}		
+	}
+	
+	if(strcmp(argv[0],"tmpsys") == 0)
+	{		
+		cmd = "pkmt lfs tmpsys --version ";
+		cmd += version.toString();
+		return shell.execute(cmd);
+	}
+	else if(strcmp(argv[0],"basic") == 0)
+	{		
+		cmd = "pkmt lfs basic --version ";
+		cmd += version.toString();
+		return shell.execute(cmd);
+	}
+	
+	return 0;
+}
+int Interpret::remove(int argc, char* argv[])
 {
 	std::string cmd = "bdt-prephost-remove ";
 	cmd = cmd + (((pkmt::HeaderLFS*)header)->getLFS_PART()) + " " + (((pkmt::HeaderLFS*)header)->getLFS());
 	//std::cout << "Ejecutando : "<< cmd << "\n";
 	return system (cmd.c_str());
 }
-int LFS::install(int argc, char* argv[])
+int Interpret::install(int argc, char* argv[])
 {
 	std::string cmd;
 	#ifdef DEBUG
@@ -66,6 +175,9 @@ int LFS::install(int argc, char* argv[])
 		{
 			ver.set(argv[1]);
 			((pkmt::HeaderLFS*)header)->setVersion(ver);
+			std::string cmdver = "echo ";
+			cmdver += ver.toString() + " >> " +  header->getRootDir() + "/etc/version";
+			shell.execute(cmdver);
 		}
 	}
 	cmd = cmd + (((pkmt::HeaderLFS*)header)->getLFS_PART()) \
@@ -86,7 +198,7 @@ int LFS::install(int argc, char* argv[])
 	//std::cout << "Ejecutando : "<< cmd << "\n";
 	return system (cmd.c_str());
 }
-int LFS::prephost(int argc, char* argv[])
+int Interpret::prephost(int argc, char* argv[])
 {
 	if(strcmp(argv[0],"install") == 0)
 	{
@@ -98,7 +210,11 @@ int LFS::prephost(int argc, char* argv[])
 	}
 	else if(strcmp(argv[0],"sync") == 0)
 	{
-		return sync(argc-1,argv+1);
+		//return sync(argc-1,argv+1);
+	}
+	else if(strcmp(argv[0],"build") == 0)
+	{
+		return build(argc-1,argv+1);
 	}
 	else
 	{
@@ -106,20 +222,9 @@ int LFS::prephost(int argc, char* argv[])
 		msg = msg + "'" + argv[0] + "' commando desconocida.";
 		throw msg;
 	}
+	
+	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 Interpret::Interpret(const pkmt::Header& h)
 {
 	this->header = &h;
@@ -144,6 +249,18 @@ int Interpret::bdt(int argc, char* argv[])
 	{
 		LFS lfs(*header);
 		return lfs.prephost(argc-1,argv+1);
+	}
+	else if(strcmp(argv[0],"--help") == 0)
+	{
+		std::cout << "bdt prephost remove				Elimnano el host\n";
+		std::cout << "bdt prephost install --version X	Crea un host para la version X\n";
+		std::cout << "bdt prephost build tmpsys	--version X		Construlle el sistem temporal X\n";
+		std::cout << "bdt prephost config1 --version X			Construlle el sistem temporal X\n";
+		std::cout << "bdt prephost config2 --version X			Construlle el sistem temporal X\n";
+		std::cout << "bdt prephost config3 --version X			Construlle el sistem temporal X\n";
+		std::cout << "bdt prephost build basic --version X		Construlle el sistem temporal X\n";
+		
+		return 0;
 	}
 	else
 	{
--- src/commands.hh
+++ src/commands.hh
@@ -3,7 +3,7 @@
 #define BDT_COMMANDS
 
 #include <octetos/coreutils/shell.hh>
-
+#include <octetos/core/Version.hh>
 #include <pkmt/header.hh>
 
 
@@ -16,13 +16,22 @@ class Interpret
 {
 protected:
 	const pkmt::Header* header;
-	//
+	
 private:
 	int bdt(int argc, char* argv[]);
+	coreutils::Shell shell;
+	octetos::core::Semver version;
 	
 public:
 	Interpret(const pkmt::Header& configure);
 	int execute(int argc, char* argv[]);
+	int prephost(int argc, char* argv[]);
+	int remove(int argc, char* argv[]);
+	int install(int argc, char* argv[]);
+	int build(int argc, char* argv[]);
+	int config1(int argc, char* argv[]);
+	int config2(int argc, char* argv[]);
+	int config3(int argc, char* argv[]);
 };
 
 class LFS : public Interpret
@@ -30,9 +39,6 @@ class LFS : public Interpret
 	
 public:
 	LFS(const pkmt::Header& h);
-	int prephost(int argc, char* argv[]);
-	int install(int argc, char* argv[]);
-	int remove(int argc, char* argv[]);
 	int sync(int argc, char* argv[]);
 	int download(int argc, char* argv[]);
 };
